c739df2b723a1118e1c1783b6ae88e68041e5726
JiaMingLiu93 <626345665@qq.com>
2018-08-15 06:32:45 +0800

    add test modules and test validation

---
+++ test/src/main/java/com/demo/validate/UserTest.java
@@ -0,0 +1,28 @@
+package com.demo.validate;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+/**
+ * @author jam
+ * @description
+ * @create 2018-07-27
+ **/
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setWorking(true);
+        user.setAboutMe("Its all about me!");
+        user.setAge(50);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        for (ConstraintViolation<User> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+    }
+}
