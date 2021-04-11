5ee85d6b31cc1bdca1e9978d4d7c160a08ec4072
JiaMingLiu93 <626345665@qq.com>
2021-04-11 18:42:58 +0800

    feat: 1.add spring-test

---
+++ springdiscover/spring-test/spring-mvc/src/main/java/com/homegrown/spring/discover/test/mvc/beans/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.homegrown.spring.discover.test.mvc.beans.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index.html")
+    public String index(){
+        return "index";
+    }
+}
